9b7e27c6f92c19b1d0addf84b1e16e111d7f0217
Fastyyk <116104906+Fastyyk@users.noreply.github.com>
2023-05-03 21:45:33 +0300

    12345

---
+++ script.js
@@ -0,0 +1,18 @@
+
+
+let number = (4, 7, 2, 8, 4, 5,);
+let sun = 0;
+
+for (let 1 = 0; 1 < number.length; 1++) (
+    sun += number[1];
+)
+let average = sun / number length;
+console.log("Середнє значення масиву", average )
+
+let max = number[0];
+for (let i = 1; <number.length i++) {
+    if (number[1] > max) {
+        max = number[i]
+    }
+}
+console.log("Елемент з найбільшим числом")
